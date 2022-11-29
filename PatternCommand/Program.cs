c4638aeb777bbf31d76f7f2d789e9588cf158cd5
admina <it-mactep@mail.ru>
2022-11-29 18:07:17 +0500

    v2

--- PatternCommand/Program.cs
+++ PatternCommand/Program.cs
@@ -1,4 +1,7 @@
-﻿using YoutubeExplode;
+﻿using System;
+using ShellProgressBar;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
 
 namespace PatternCommand;
 
@@ -8,45 +11,77 @@ interface ICommand
     void Undo();
 }
 
-
-
 class VideoPlayer
 {
+    public string videoUrl { private get; set;}
+    public string Title { get; private set;}
+    public string Description { get; private set;}
     YoutubeClient youtube = new YoutubeClient();
+    
 
-    public async Task GetInfoAsync()
+    public async Task GetVideoInfoAsync()
     {
-        Console.WriteLine("Введите Url video:");
-        string videoUrl = Console.ReadLine();
-        if (videoUrl != null)
+        //Пробуем получить инфо
+        try
         {
-            try
-            {
-                // You can specify both video ID or URL
-                var video = await youtube.Videos.GetAsync(videoUrl);
+            // You can specify both video ID or URL
+            var video = await youtube.Videos.GetAsync(videoUrl);
 
-                var title = video.Title; // "Collections - Blender 2.80 Fundamentals"
-                                         //var author = video.Author.ChannelTitle; // "Blender"
-                var description = video.Description; // desc
-                Console.WriteLine("Title: " + title);
-                Console.WriteLine("Description: " + description);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Title = video.Title; // "Collections - Blender 2.80 Fundamentals"
+            //var author = video.Author.ChannelTitle; // "Blender"
+            Description = video.Description; // desc
+            Console.WriteLine();
+            Console.WriteLine("============================");
+            Console.WriteLine("Title:");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+            Console.WriteLine(Title);
+            Console.WriteLine();
+            Console.WriteLine("============================");
+            Console.WriteLine("Description:");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+            Console.WriteLine(Description);
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("Вы не ввели Url video! Попробуйте снова.");
+            Console.WriteLine(ex.Message);
         }
-
     }
 
+    public async Task DownloadVideoAsync()
+    {
+        // You can specify both video ID or URL
+        //var video = await youtube.Videos.GetAsync("https://youtube.com/watch?v=u_yIGGhubZs");
+        try
+        {
+            Console.WriteLine();
+            Console.WriteLine("============================");
+            Console.WriteLine("Скачиваю видео:");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+            Console.WriteLine(Title);
+            //var progress = new Progress<double>
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+            // Get highest quality muxed stream
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+            // Download the stream to a file
+            //Progress<double> pb = new Progress<double>(p => Console.WriteLine($"Progress updated: {p}"));
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
+            Console.WriteLine();
+            Console.WriteLine("============================");
+            Console.WriteLine("Скачивание завершено.");
+            Console.WriteLine("============================");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
     public void Off()
     {
         Console.WriteLine("Телевизор выключен...");
-    }
+    }   
 }
 
 class GetVideoInfoCommand : ICommand
@@ -58,7 +93,24 @@ class GetVideoInfoCommand : ICommand
     }
     public async Task ExecuteAsync()
     {
-        await videoPlayer.GetInfoAsync();
+        await videoPlayer.GetVideoInfoAsync();
+    }
+    public void Undo()
+    {
+        videoPlayer.Off();
+    }
+}
+
+class DownloadVideoCommand : ICommand
+{
+    VideoPlayer videoPlayer;
+    public DownloadVideoCommand(VideoPlayer videoPlayer)
+    {
+        this.videoPlayer = videoPlayer;
+    }
+    public async Task ExecuteAsync()
+    {
+        await videoPlayer.DownloadVideoAsync();
     }
     public void Undo()
     {
@@ -87,6 +139,8 @@ class Sender
 }
 
 
+
+
 class Program
 {
     static async Task Main()
@@ -100,8 +154,13 @@ class Program
         // создадим получателя 
         var videoPlayer = new VideoPlayer();
 
+        // получить url
+        Console.WriteLine("Введите Url видео:");
+        videoPlayer.videoUrl = Console.ReadLine();
+
         // создадим команду 
         var getVideoInfoCommand = new GetVideoInfoCommand(videoPlayer);
+        var downloadVideoCommand = new DownloadVideoCommand(videoPlayer);
 
         // инициализация команды
         sender.SetCommand(getVideoInfoCommand);
@@ -109,5 +168,10 @@ class Program
         //  выполнение
         await sender.Run();
 
+        // инициализация команды
+        sender.SetCommand(downloadVideoCommand);
+
+        //  выполнение
+        await sender.Run();
     }
 }
